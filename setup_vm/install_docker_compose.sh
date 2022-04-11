d90a82306284ff972a6326dc98e564bb0c2dc4b7
Kyllian Beguin <50613619+KyllianBeguin@users.noreply.github.com>
2022-04-11 19:42:32 +0200

    inital commit
    To install docker compose on Debian

---
+++ setup_vm/install_docker_compose.sh
@@ -0,0 +1,6 @@
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
