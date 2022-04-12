c066dd1991149f8fa05613c7243b7d8399ae7e7e
Kyllian Beguin <50613619+KyllianBeguin@users.noreply.github.com>
2022-04-12 20:33:38 +0200

    Update install_docker.sh

--- setup_vm/install_docker.sh
+++ setup_vm/install_docker.sh
@@ -12,6 +12,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt-get update
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo docker run hello-world
