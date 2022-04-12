04e7213ab50489db437dd7b0d7a79a91acbfd860
Kyllian Beguin <50613619+KyllianBeguin@users.noreply.github.com>
2022-04-12 09:10:13 +0200

    Delete install_kafdrop.sh

--- setup_vm/install_kafdrop.sh
+++
@@ -1,5 +0,0 @@
-sudo docker run -d --rm -p 9000:9000 \
-    -e KAFKA_BROKERCONNECT=host:port,host:port \
-    -e JVM_OPTS="-Xms32M -Xmx64M" \
-    -e SERVER_SERVLET_CONTEXTPATH="/" \
-    obsidiandynamics/kafdrop:latest
