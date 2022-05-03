792c22ecf7c4781303c70f29fa0ab25bb99debc8
Kyllian Beguin <50613619+KyllianBeguin@users.noreply.github.com>
2022-05-03 20:35:40 +0200

    Update setup_mongo_coopmilk.js

--- setup_vm/DockerCompose/Mongo/setup_mongo_coopmilk.js
+++ setup_vm/DockerCompose/Mongo/setup_mongo_coopmilk.js
@@ -1,2 +1,6 @@
 use coopmilk
-db.createCollection("usine1_wallah")
+db.createCollection("usine1")
+db.createCollection("usine2")
+db.createCollection("usine3")
+db.createCollection("usine4")
+db.createCollection("usine5")
